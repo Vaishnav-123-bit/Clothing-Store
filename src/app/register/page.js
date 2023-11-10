49f9217cac9fae9432141146f8dad4163932751d
vaishnav mahdik <1032210456@tcetmumbai.in>
2023-11-10 13:08:36 +0530

    a

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -2,79 +2,102 @@
 
 import InputComponent from "@/components/FormElements/InputComponent";
 import SelectComponent from "@/components/FormElements/SelectComponent";
-import { registrationFormControls } from "@/utils";
-import { useState } from "react";
 
-const isRegistered = false;
-const initialFormdata = {
+
+import { GlobalContext } from "@/context";
+
+import { registrationFormControls } from "@/utils";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
+
+
+const initialFormData = {
   name: " ",
-  email: " ",
+  email: " ", 
   password: " ",
   role: "customer",
 };
+
 export default function Register() {
-  const [formData, setFormData] = useState(initialFormdata);
+  const [formData, setFormData] = useState(initialFormData);
+  const [isRegistered, setIsRegistered] = useState(false);
+  const { pageLevelLoader, setPageLevelLoader , isAuthUser } = useContext(GlobalContext);
+
+  const router = useRouter()
+
   console.log(formData);
 
   function isFormValid() {
     return formData &&
       formData.name &&
-      formData.name.trim() !== '' &&
+      formData.name.trim() !== "" &&
       formData.email &&
-      formData.email.trim() !== '' &&
+      formData.email.trim() !== "" &&
       formData.password &&
-      formData.password.trim() !== ''
+      formData.password.trim() !== ""
       ? true
       : false;
   }
+
+  console.log(isFormValid());
+
+
   return (
     <div className="bg-white relative">
-      <div className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-8 mr-auto  xl:px-5 lg:flex-row">
+      <div className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-8 mr-auto xl:px-5 lg:flex-row">
         <div className="flex flex-col justify-center items-center w-full pr-10 pl-10 lg:flex-row">
-          <div className="w-full mt-10 mr-0 mb-0 ml-0 relative max-w-2xl lg:mt-0 lg:w-5/12 ">
-            <div className="flex flex-col items-center justify-start pt-10 pr-10 pb-10 pl-10 bg-white shadow-2xl rounded-xl  relative z-10">
+          <div className="w-full mt-10 mr-0 mb-0 ml-0 relative max-w-2xl lg:mt-0 lg:w-5/12">
+            <div className="flex flex-col items-center justify-start pt-10 pr-10 pb-10 pl-10 bg-white shadow-2xl rounded-xl relative z-10">
               <p className="w-full text-4xl font-medium text-center font-serif">
                 {isRegistered
-                  ? "Registration sucessful"
-                  : "sign up for an account"}
+                  ? "Registration Successfull !"
+                  : "Sign up for an account"}
               </p>
               {isRegistered ? (
-                <button className="inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg text-white transition-all duration-200 ease-in-out focus:shadow font-medium uppercase traking-wide">
+                <button
+                  className="inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg 
+                text-white transition-all duration-200 ease-in-out focus:shadow font-medium uppercase tracking-wide
+                "
+                onClick={()=>router.push('/login')}
+                >
                   Login
                 </button>
               ) : (
                 <div className="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
-                  {registrationFormControls.map((controlItems) =>
-                    controlItems.componentType === "input" ? (
+                  {registrationFormControls.map((controlItem) =>
+                    controlItem.componentType === "input" ? (
                       <InputComponent
-                        type={controlItems.type}
-                        placeholder={controlItems.placeholder}
-                        label={controlItems.label}
+                        type={controlItem.type}
+                        placeholder={controlItem.placeholder}
+                        label={controlItem.label}
                         onChange={(event) => {
                           setFormData({
                             ...formData,
-                            [controlItems.id]: event.target.value,
+                            [controlItem.id]: event.target.value,
                           });
                         }}
-                        value={formData[controlItems.id]}
+                        value={formData[controlItem.id]}
                       />
-                    ) : controlItems.componentType === "select" ? (
+                    ) : controlItem.componentType === "select" ? (
                       <SelectComponent
-                        options={controlItems.options}
-                        label={controlItems.label}
+                        options={controlItem.options}
+                        label={controlItem.label}
                         onChange={(event) => {
                           setFormData({
                             ...formData,
-                            [controlItems.id]: event.target.value,
+                            [controlItem.id]: event.target.value,
                           });
                         }}
-                        value={formData[controlItems.id]}
+                        value={formData[controlItem.id]}
                       />
                     ) : null
                   )}
                   <button
+                    className=" disabled:opacity-50 inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg 
+                   text-white transition-all duration-200 ease-in-out focus:shadow font-medium uppercase tracking-wide
+                   "
                     disabled={!isFormValid()}
-                    className="disables:opacity-50 inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg text-white transition-all duration-200 ease-in-out focus:shadow font-medium uppercase traking-wide"
+                    
                   >
                     Register
                   </button>
@@ -84,6 +107,7 @@ export default function Register() {
           </div>
         </div>
       </div>
+      
     </div>
   );
 }
--- src/database/index.js
+++ src/database/index.js
@@ -7,12 +7,11 @@ const configOptions = {
 const connectToDb = async () => {
   const connectionUrl = "mongodb+srv://hard:hard@cluster0.sucrsem.mongodb.net/";
 
-  (await mongoose.connect(connectionUrl, configOptions))
-    .isObjectIdOrHexString(() =>
-      console.log("ecom database connected sucessfully !!")
-    )
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Ecommerce database connected successfully!"))
     .catch((err) =>
-      console.log(`getting error in db connection ${err.message}`)
+      console.log(`Getting Error from DB connection ${err.message}`)
     );
 };
 
